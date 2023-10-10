ecee279ec3dd657f352bdf7239ecf7cfe55f9d00
Lautaro <103766108+Lautaroscu@users.noreply.github.com>
2023-10-10 14:56:50 -0300

    Update adqurido.js

--- src/components/adqurido.js
+++ src/components/adqurido.js
@@ -1,8 +1,8 @@
 export const getFlagAdquirido = () => {
   return ` 
   <div class="adquirido">
-  <img src="/src/assets/svgs/Group 39.svg" alt="">
-  <img  class="elipse" src="/src/assets/svgs/Ellipse 4.svg" alt="">
+  <img src="./src/assets/svgs/Group 39.svg" alt="">
+  <img  class="elipse" src="./src/assets/svgs/Ellipse 4.svg" alt="">
   <p>ADQUIRIDO</p>
 </div>
 `;
